fd5b4e4fc6cb1def3220d357d99e170b60f43ae1
Shaurya <agarwal.lucky.shaurya11@gmail.com>
2021-05-04 20:30:06 +0530

    fucked up commit history

--- utils/sendgrid.go
+++ utils/sendgrid.go
@@ -9,14 +9,20 @@ import (
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
+var (
+	SENDGRID_API_KEY = os.Getenv("SENDGRID_API_KEY")
+)
+
 func SendEmail() {
 	from := mail.NewEmail("Example User", "test@example.com")
-	subject := "Sending with Twilio SendGrid is Fun"
+	subject := "Confirm Your Email!"
 	to := mail.NewEmail("Example User", "test@example.com")
 	plainTextContent := "and easy to do anywhere, even with Go"
 	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
 	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
-	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+
+	client := sendgrid.NewSendClient(SENDGRID_API_KEY)
+
 	response, err := client.Send(message)
 	if err != nil {
 		log.Println(err)
